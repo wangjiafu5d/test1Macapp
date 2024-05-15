85a097ac4c831e6a6e0930ce1048b670c4b167f4
wangjiafu5d <wangjiafu5d@163.com>
2024-05-15 16:01:13 +0800

    Initial Commit

---
+++ test1Macapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test1Macapp
+//
+//  Created by Devcloud on 2024/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test1Macapp/test1MacappApp.swift
@@ -0,0 +1,17 @@
+//
+//  test1MacappApp.swift
+//  test1Macapp
+//
+//  Created by Devcloud on 2024/5/15.
+//
+
+import SwiftUI
+
+@main
+struct test1MacappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
